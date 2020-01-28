62f38531e60f9ad3ae7918d901faa621f7bc7c12
Quinn Krug <quinn.krug@gmail.com>
2020-01-27 19:27:13 -0700

    intial form built

---
+++ app/controllers/team_controller.rb
@@ -0,0 +1,17 @@
+class TeamController < ApplicationController
+
+  def index
+    @champ_names = champ_names
+  end
+
+  private
+
+  def champ_names
+    def conn
+      Faraday.new(url: 'http://ddragon.leagueoflegends.com/cdn/10.2.1/data/en_US/champion.json').get
+    end
+
+    JSON.parse(conn.body, symbolize_names: true)[:data].keys
+  end
+
+end
