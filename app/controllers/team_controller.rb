a87f69646c344edcc2cd55e29aacc499b0d8fdb7
Quinn Krug <quinn.krug@gmail.com>
2020-01-29 19:53:58 -0700

    passing champ names through form

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -5,6 +5,11 @@ class TeamController < ApplicationController
   end
 
   def enemy
+    @champ_1 = champ_data(params[:champ1])
+    @champ_2 = champ_data(params[:champ2])
+    @champ_3 = champ_data(params[:champ3])
+    @champ_4 = champ_data(params[:champ4])
+    @champ_5 = champ_data(params[:champ5])
     binding.pry
   end
 
@@ -16,6 +21,16 @@ class TeamController < ApplicationController
     end
 
     JSON.parse(conn.body, symbolize_names: true)[:data].keys
+  end
+
+  def champ_data(champ_name)
+
+    def conn(champ_name)
+      url = "http://ddragon.leagueoflegends.com/cdn/10.2.1/data/en_US/champion/" + champ_name + ".json"
+      Faraday.new(url: url).get
+    end
+
+    JSON.parse(conn(champ_name).body, symbolize_names: true)
   end
 
 end
